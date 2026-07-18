00ae908b17a14de6275d126a5400f30778b10d3b
SNK634 <ptam4207@gmail.com>
2026-07-18 21:31:44 +0700

    Hoan thien giao dien Core UI va form Them Nhan Vien

---
+++ ViewModels/Core/MainViewModel.cs
@@ -0,0 +1,12 @@
+using QL_quanCafe.ViewModels.Common;
+
+namespace QL_quanCafe.ViewModels.Core
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            // Logic điều hướng sẽ viết ở đây
+        }
+    }
+}
